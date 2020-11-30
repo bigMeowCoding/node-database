8db2ec336494a2bda73096429c7919f420140e76
zhouyijun <zhou495248579@hotmail.com>
2020-11-30 22:17:31 +0800

    sequelize

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize("zhouyijun", "root", "123456", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+
+class Person extends Model {}
+Person.init({
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE
+}, { sequelize, modelName: 'user' });
+//
+// (async () => {
+//     await sequelize.sync();
+//     const jane = await Person.create({
+//         username: 'janedoe',
+//         birthday: new Date(1980, 6, 20)
+//     });
+// })();
+
+async function query() {
+    const person =  await Person.findAll()
+    console.log(JSON.stringify(person))
+}
+// query()
+async function del() {
+    await Person.destroy({
+        where: {
+            id: 1
+        }
+    });
+}
+del()
